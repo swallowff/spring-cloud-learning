dc7d163bbf106f819cf2cbf9eba14ff9500d7fb1
swallowff <swallowff@163.com>
2019-10-10 09:34:55 +0800

    modify service modules

---
+++ service-order/src/main/java/cn/swallow/service/order/ctrl/RibbonCtrl.java
@@ -0,0 +1,30 @@
+package cn.swallow.service.order.ctrl;
+
+import cn.swallow.service.order.service.RibbonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ribbon负载均衡
+ * @author swallowff
+ * @create 2019/10/9
+ */
+@RestController
+@RequestMapping(value = "/api/ribbon")
+public class RibbonCtrl {
+    @Autowired
+    private RibbonService ribbonService;
+
+    /**
+     * ribbon负载均衡,将请求转发至注册的/doTest接口
+     * @param name
+     * @return
+     */
+    @RequestMapping("/testRibbon")
+    public String testRibbon(@RequestParam String name){
+        return ribbonService.doTest(name);
+    }
+
+}
